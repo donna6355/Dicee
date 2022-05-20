4d548fe39947780e3f45cfb48708e8da984b5908
Donna <donnajun1214@gmail.com>
2022-05-20 11:51:52 +0900

    add imageLiteral and variable, collection

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,15 +12,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("botton got pressed")
+        //#imageLiteral(
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        leftDiceNumber = leftDiceNumber + 1
         
-        diceImageView1.alpha = 0.5
-        diceImageView2.alpha = 0.2
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        rightDiceNumber = rightDiceNumber - 1
+        
     }
     
 }
